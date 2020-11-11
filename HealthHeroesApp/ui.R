ff6e73f5be381b21075dc4debee1830db6fce376
Wayne Loh <wayneloh.qiao.zhang@gmail.com>
2020-11-12 00:36:02 +0800

    Create new section for data

--- HealthHeroesApp/ui.R
+++ HealthHeroesApp/ui.R
@@ -1,10 +1,53 @@
 # IMPORTS
 #===================================================================================================
-packages = c('shiny','shinythemes','leaflet','shinycssloaders')
+packages = c('shiny','shinythemes','leaflet','shinycssloaders','DT')
 for (p in packages){
     library(p,character.only = T)
 }
 #===================================================================================================
+
+# SUMMARY
+#===================================================================================================
+summaryUI <- sidebarLayout(
+    sidebarPanel(
+        selectInput("summaryPointSelectAmenity", 
+                    "Amenity Type:",
+                    c("Gyms" = "gym",
+                      "Eateries" = "eat",
+                      "Places" = "places")
+        ),
+
+    ),
+    mainPanel(
+        tabsetPanel(type = "tabs",
+                    tabPanel("Data Table", 
+                             style = "overflow-y:scroll; max-height: 400px",
+                             dataTableOutput("summaryDataTable") %>% withSpinner(color="#0dc5c1")
+                             ),
+                    tabPanel("Point Map", leafletOutput("summaryPointMap") %>% withSpinner(color="#0dc5c1"))
+        )
+    )
+)
+#===================================================================================================
+
+
+# CHOROPLETH
+#===================================================================================================
+choroplethUI <- sidebarLayout(
+    sidebarPanel(
+        selectInput("chloroplethMapSelectAmenity",
+                    "Amenity Type:",
+                    c("Gyms" = "gym",
+                      "Eateries" = "eat",
+                      "Places" = "places")
+        ),  
+    ),
+    mainPanel(
+        leafletOutput("choroplethMap") %>% withSpinner(color="#0dc5c1")
+    )
+)
+#===================================================================================================
+
 
 
 # KERNEL DENSITY
@@ -26,26 +69,29 @@ kernelDensityUI <- sidebarLayout(
                       "North-East Region" = "NORTH-EAST REGION",
                       "North Region" = "NORTH REGION")
         ),
-        selectInput("kernelDensitySelectBandwidth", 
-                    "Selected Bandwidth:",
-                    c("Manual (Fastest)" = "manual",
-                      "Adaptive" = "adaptive",
-                      "bw.diggle" = "bw.diggle",
-                      "bw.scott" = "bw.scott",
-                      "bw.ppl" = "bw.ppl")
+        conditionalPanel(
+            condition = "input.tabselected == 1",
+            selectInput("kernelDensitySelectBandwidth", 
+                        "Selected Bandwidth:",
+                        c("Manual (Fastest)" = "manual",
+                          "Adaptive" = "adaptive",
+                          "bw.diggle" = "bw.diggle",
+                          "bw.scott" = "bw.scott",
+                          "bw.ppl" = "bw.ppl")
+            ),
         ),
         conditionalPanel(
-            condition = "input.kernelDensitySelectBandwidth == 'manual'",
+            condition = "input.kernelDensitySelectBandwidth == 'manual' && input.tabselected == 1",
             sliderInput("kernelDensitySliderBandwidth",
                         "Bandwidth",
                         min = 100,
-                        max = 1000,
+                        max = 2000,
                         value = 600,
                         ticks = F
             ),  
         ),
         conditionalPanel(
-            condition = "input.kernelDensitySelectBandwidth != 'adaptive'",
+            condition = "input.kernelDensitySelectBandwidth != 'adaptive' && input.tabselected == 1",
             selectInput("kernelDensitySelectKernelMethod", 
                         "Kernel Method:",
                         c("Gaussian" = "gaussian",
@@ -56,7 +102,12 @@ kernelDensityUI <- sidebarLayout(
         )
     ),
     mainPanel(
-        plotOutput("kernelDensityMap") %>% withSpinner(color="#0dc5c1")
+        tabsetPanel(type = "tabs",
+                    id = "tabselected",
+                    # tabPanel("Kernel Density Map", leafletOutput("kernelDensityMap") %>% withSpinner(color="#0dc5c1")),
+                    tabPanel("Kernel Density Map", value=1, plotOutput("kernelDensityMap") %>% withSpinner(color="#0dc5c1")),
+                    tabPanel("Clark-Evans Test (Takes a while)", value=2, tableOutput("kernelDensityClarkEvans") %>% withSpinner(color="#0dc5c1"))
+        )
     )
 )
 #===================================================================================================
@@ -87,7 +138,7 @@ secondOrderUI <- sidebarLayout(
     mainPanel(
         tabsetPanel(type = "tabs",
                     tabPanel("Function Estimation", plotOutput("secondOrderEstimationPlot") %>% withSpinner(color="#0dc5c1")),
-                    tabPanel("Complete Spatial Randomness Test", plotOutput("secondOrderCompleteSpatRandPlot") %>% withSpinner(color="#0dc5c1"))
+                    tabPanel("Complete Spatial Randomness Test (Takes a while)", plotOutput("secondOrderCompleteSpatRandPlot") %>% withSpinner(color="#0dc5c1"))
         )
     )
 )
@@ -120,7 +171,9 @@ accessibilityUI <- sidebarLayout(
 shinyUI(navbarPage(
     "HealthHeroes",
     theme = shinytheme("yeti"),
-    tabPanel(title = "First Order (Kernel Density) Analysis", value = "kernelDensity", kernelDensityUI),
+    tabPanel(title = "Data", value = "summary", summaryUI),
+    tabPanel(title = "Choropleth Mapping", value = "choropleth", choroplethUI),
+    tabPanel(title = "First Order Analysis", value = "kernelDensity", kernelDensityUI),
     tabPanel(title = "Second Order Analysis", value = "secondOrder", secondOrderUI),
     tabPanel(title = "Accessibility Analysis", value = "accessibility", accessibilityUI)
 ))
